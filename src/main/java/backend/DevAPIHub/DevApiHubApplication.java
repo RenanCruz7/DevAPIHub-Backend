d4d91637ee2cfacbf0e0c27f6e6a58a45e0f442b
RenanCruz7 <renancruz199@gmail.com>
2024-09-05 17:33:10 -0300

    Initial Commit

---
+++ src/main/java/backend/DevAPIHub/DevApiHubApplication.java
@@ -0,0 +1,13 @@
+package backend.DevAPIHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevApiHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevApiHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/DevAPIHub/DevApiHubApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.DevAPIHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevApiHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
