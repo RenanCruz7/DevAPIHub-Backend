e3049761d4c0467a13cf0dfc117a70149aa10755
RenanCruz7 <renancruz199@gmail.com>
2024-09-05 18:17:08 -0300

    Criando entidades

---
+++ src/main/java/backend/DevAPIHub/domain/hub/Hub.java
@@ -0,0 +1,38 @@
+package backend.DevAPIHub.domain.hub;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "hub")
+@Entity(name = "hub")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Hub {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String apiName;
+    private String apiDescription;
+    private boolean authRequired;
+    private String apiLink;
+    private String apiCategory;
+    @Min(1)
+    @Max(5)
+    private float rating;
+
+    public Hub(String apiName, String apiDescription, boolean authRequired, String apiLink, String apiCategory, @Min(1) @Max(5) float rating) {
+        this.apiName = apiName;
+        this.apiDescription = apiDescription;
+        this.authRequired = authRequired;
+        this.apiLink = apiLink;
+        this.apiCategory = apiCategory;
+        this.rating = rating;
+    }
+}
