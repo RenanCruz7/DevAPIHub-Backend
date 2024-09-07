62c848c8407452c1cd38422c5a06fba934f7fd5b
RenanCruz7 <renancruz199@gmail.com>
2024-09-07 12:44:30 -0300

    Começando autenticaçao JWT

---
+++ src/main/java/backend/DevAPIHub/infra/security/TokenService.java
@@ -0,0 +1,49 @@
+package backend.DevAPIHub.infra.security;
+
+import backend.DevAPIHub.domain.user.User;
+import lombok.Value;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("api.security.token.secret")
+    private String secret;
+    public String GenerateToken(User user){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("Sistema de Blog")
+                    .withSubject(user.getUsername())
+                    .withExpiresAt(ExpirationDate())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("erro ao gerrar token jwt", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("Sistema de Blog")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
+        }
+    }
+
+    private Instant ExpirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
